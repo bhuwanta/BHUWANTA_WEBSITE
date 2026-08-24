2128905ff1a194da38205d0a4a6a18e7b2412bb6
suroju-hom-sai-santosh <surojuhomsaisantosh@gmail.com>
2026-08-24 20:30:26 +0530

    Resolved the emails sent to users

--- test_wings.ts
+++
@@ -1,7 +0,0 @@
-import { getWingsListAction } from './src/app/(REALESTATE_SOFTWARE)/REALESTATE_SOFTWARE/role/it/modules/user_management/actions';
-
-async function run() {
-  const res = await getWingsListAction();
-  console.log(JSON.stringify(res, null, 2));
-}
-run();
